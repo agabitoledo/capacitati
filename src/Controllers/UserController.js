e82f842ace107c378e544c5e1f8326cffcb1a1d0
agabitoledo <gabriellefariat@gmail.com>
2022-02-08 23:49:40 -0300

    Criada duas rotas (Get Users e Create User)

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,22 +1,44 @@
+const db = require('../../db');
+
+//Create User
 exports.post = (req, res, next) => {
-    res.status(201).send("Rota Post");
+   const body = req.body;
+   db("users").insert(body).then((data => {
+      console.log("body", body);
+      console.log("data", data);
+      return (
+         res.status(200).send({
+            ...body,
+            id: data,
+         })
+      )
+   }))
 }
 
+//Update User
 exports.put = (req, res, next) => {
-    let id = req.params.id;
-    res.status(201).send(`Rota PUT com ID! --> ${id}`);
- };
-  
- exports.delete = (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send(`Rota DELETE com ID! --> ${id}`);
- };
-  
- exports.get = (req, res, next) => {
-    res.status(200).send('Rota GET!');
- };
-  
- exports.getById = (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send(`Rota GET com ID! ${id}`);
- };
+   let id = req.params.id;
+   res.status(201).send(`Rota PUT com ID! --> ${id}`);
+};
+
+//Delete User
+exports.delete = (req, res, next) => {
+   let id = req.params.id;
+   res.status(200).send(`Rota DELETE com ID! --> ${id}`);
+};
+
+//Get User list
+exports.get = (req, res, next) => {
+   db.select().table("users").then(data => {
+      console.log(data)
+      return (
+         res.status(200).send(JSON.stringify(data))
+      )
+   })
+};
+
+//Get User by Id
+exports.getById = (req, res, next) => {
+   let id = req.params.id;
+   res.status(200).send(`Rota GET com ID! ${id}`);
+};
